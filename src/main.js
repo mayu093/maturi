a45a8df83474c229ff245a388392396266b144a6
mayu093 <24mv093@st.nuas.ac.jp>
2026-07-24 08:45:49 +0900

    update content

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 // JavaScriptのメインファイル
 console.log("main.js loaded");
+
+function openModal() {
+  document.getElementById("ticket-modal").classList.add("active");
+}
+
+function closeModal() {
+  document.getElementById("ticket-modal").classList.remove("active");
+}
